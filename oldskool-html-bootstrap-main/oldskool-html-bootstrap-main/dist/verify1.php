870ab409f289dbe32a17e1b973d78c1389b1cf5e
halusinasi4ri <arianarissaa04@gmail.com>
2024-10-07 01:31:52 +0800

    verified-email

--- oldskool-html-bootstrap-main/oldskool-html-bootstrap-main/dist/verify1.php
+++ oldskool-html-bootstrap-main/oldskool-html-bootstrap-main/dist/verify1.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli('localhost', 'root', '', 'your_database');
+$conn = new mysqli('localhost', 'root', '', 'meqa.my');
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
